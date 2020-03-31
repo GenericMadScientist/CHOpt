8e7d23eccf6e4857bc7cd6406ed317d45ea74d51
GenericMadScientist <rayw1710@gmail.com>
2020-03-31 09:52:17 +0100

    Move TimeConverter to time.cpp
    This also necessitates moving the directly relevant tests to
    time_unittest.cpp.

---
+++ src/time.hpp
@@ -0,0 +1,56 @@
+/*
+ * chopt - Star Power optimiser for Clone Hero
+ * Copyright (C) 2020 Raymond Wright
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CHOPT_TIME_HPP
+#define CHOPT_TIME_HPP
+
+#include <vector>
+
+#include "chart.hpp"
+
+class TimeConverter {
+private:
+    struct BeatTimestamp {
+        double beat;
+        double time;
+    };
+
+    struct MeasureTimestamp {
+        double measure;
+        double beat;
+    };
+
+    static constexpr double MS_PER_MINUTE = 60000.0;
+    static constexpr uint32_t DEFAULT_BPM = 120000;
+    static constexpr double DEFAULT_BEAT_RATE = 4.0;
+    std::vector<BeatTimestamp> m_beat_timestamps;
+    std::vector<MeasureTimestamp> m_measure_timestamps;
+    double m_last_beat_rate;
+    uint32_t m_last_bpm;
+
+public:
+    TimeConverter(const SyncTrack& sync_track, const SongHeader& header);
+    [[nodiscard]] double beats_to_seconds(double beats) const;
+    [[nodiscard]] double seconds_to_beats(double seconds) const;
+    [[nodiscard]] double beats_to_measures(double beats) const;
+    [[nodiscard]] double measures_to_beats(double measures) const;
+    [[nodiscard]] double measures_to_seconds(double measures) const;
+    [[nodiscard]] double seconds_to_measures(double seconds) const;
+};
+
+#endif
---
+++ tests/time_unittest.cpp
@@ -0,0 +1,85 @@
+/*
+ * chopt - Star Power optimiser for Clone Hero
+ * Copyright (C) 2020 Raymond Wright
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <array>
+
+#include "catch.hpp"
+
+#include "chart.hpp"
+#include "time.hpp"
+
+// Last checked: 24.0.1555-master
+TEST_CASE("Beats to seconds conversion", "Beats<->S")
+{
+    const auto track = SyncTrack({{0, 4, 4}}, {{0, 150000}, {800, 200000}});
+    const auto header = SongHeader(0.F, 200);
+    const auto converter = TimeConverter(track, header);
+    constexpr std::array beats {-1.0, 0.0, 3.0, 5.0};
+    constexpr std::array seconds {-0.5, 0.0, 1.2, 1.9};
+
+    for (auto i = 0U; i < beats.size(); ++i) {
+        REQUIRE(converter.beats_to_seconds(beats.at(i))
+                == Approx(seconds.at(i)));
+    }
+
+    for (auto i = 0U; i < beats.size(); ++i) {
+        REQUIRE(converter.seconds_to_beats(seconds.at(i))
+                == Approx(beats.at(i)));
+    }
+}
+
+// Last checked: 24.0.1555-master
+TEST_CASE("Beats to measures conversion", "Beats<->Measures")
+{
+    const auto track = SyncTrack({{0, 5, 4}, {1000, 4, 4}, {1200, 4, 16}}, {});
+    const auto header = SongHeader(0.F, 200);
+    const auto converter = TimeConverter(track, header);
+    constexpr std::array beats {-1.0, 0.0, 3.0, 5.5, 6.5};
+    constexpr std::array measures {-0.25, 0.0, 0.6, 1.125, 1.75};
+
+    for (auto i = 0U; i < beats.size(); ++i) {
+        REQUIRE(converter.beats_to_measures(beats.at(i))
+                == Approx(measures.at(i)));
+    }
+
+    for (auto i = 0U; i < beats.size(); ++i) {
+        REQUIRE(converter.measures_to_beats(measures.at(i))
+                == Approx(beats.at(i)));
+    }
+}
+
+// Last checked: 24.0.1555-master
+TEST_CASE("Measures to seconds conversion", "Measures<->S")
+{
+    const auto track = SyncTrack({{0, 5, 4}, {1000, 4, 4}, {1200, 4, 16}},
+                                 {{0, 150000}, {800, 200000}});
+    const auto header = SongHeader(0.F, 200);
+    const auto converter = TimeConverter(track, header);
+    constexpr std::array measures {-0.25, 0.0, 0.6, 1.125, 1.75};
+    constexpr std::array seconds {-0.5, 0.0, 1.2, 2.05, 2.35};
+
+    for (auto i = 0U; i < measures.size(); ++i) {
+        REQUIRE(converter.measures_to_seconds(measures.at(i))
+                == Approx(seconds.at(i)));
+    }
+
+    for (auto i = 0U; i < measures.size(); ++i) {
+        REQUIRE(converter.seconds_to_measures(seconds.at(i))
+                == Approx(measures.at(i)));
+    }
+}
