a5bbbc4ffb653e6019131b127788f51700a473fb
Raymond Wright <rayw1710@gmail.com>
2020-08-01 22:14:02 +0100

    Remove attempted fix with std::tuple<?,?>

--- src/chart.cpp
+++ src/chart.cpp
@@ -616,15 +616,10 @@ static NoteColour colour_from_key(std::uint8_t key)
 
 static bool is_open_event_sysex(const SysexEvent& event)
 {
-    constexpr std::array<std::tuple<std::size_t, int>, 6>
-        REQUIRED_BYTES {std::tuple<std::size_t, int> {0, 0x50},
-                        {1, 0x53},
-                        {2, 0},
-                        {3, 0},
-                        {5, 1},
-                        {7, 0xF7}};
-    constexpr std::array<std::tuple<std::size_t, int>, 2>
-        UPPER_BOUNDS {std::tuple<std::size_t, int> {4, 3}, {6, 1}};
+    constexpr std::array<std::tuple<std::size_t, int>, 6> REQUIRED_BYTES {
+        std::tuple {0, 0x50}, {1, 0x53}, {2, 0}, {3, 0}, {5, 1}, {7, 0xF7}};
+    constexpr std::array<std::tuple<std::size_t, int>, 2> UPPER_BOUNDS {
+        std::tuple {4, 3}, {6, 1}};
     constexpr int SYSEX_DATA_SIZE = 8;
 
     if (event.data.size() != SYSEX_DATA_SIZE) {
