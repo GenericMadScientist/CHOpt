4e0841304ce18a8354ffbdc5a8a52b6c10838cf0
Raymond Wright <rayw1710@gmail.com>
2020-08-25 06:13:23 +0100

    Use icon in main window

--- gui/main.cpp
+++ gui/main.cpp
@@ -19,11 +19,13 @@
 #include "mainwindow.hpp"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowIcon(QIcon(":/icon.png"));
     w.show();
     return a.exec();
 }
