530212ae862f6218753e8c90afbc0018019aa42d
Raymond Wright <rayw1710@gmail.com>
2020-09-12 10:25:56 +0100

    Formatting

--- src/chart.cpp
+++ src/chart.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <algorithm> 
+#include <algorithm>
 #include <charconv>
 #include <optional>
 #include <stdexcept>
