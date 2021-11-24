928f9634431c71f19066197cc735cfe680d4d2f6
Raymond Wright <rayw1710@gmail.com>
2021-11-24 16:05:14 +0000

    Add Mississippi Queen drums integration test

--- integration_tests/run_integration_tests.py
+++ integration_tests/run_integration_tests.py
@@ -8,14 +8,14 @@ conn = sqlite3.connect("integration_tests/tests.db")
 c = conn.cursor()
 
 c.execute(
-    ("select ID, File, Difficulty, Path, BaseScore, TotalScore, AvgMultiplier "
-     "from Songs")
+    ("select ID, File, Difficulty, Instrument, Path, BaseScore, TotalScore, "
+     "AvgMultiplier from Songs")
 )
 songs = list(c.fetchall())
 
 outputs = []
 for song in songs:
-    song_id, _, _, path, base_score, total_score, avg_mult = song
+    song_id, _, _, _, path, base_score, total_score, avg_mult = song
     output_lines = ["Optimising, please wait..."]
     output_lines.append(f"Path: {path}")
     output_lines.append(f"No SP score: {base_score}")
@@ -41,9 +41,10 @@ for song in songs:
 conn.close()
 
 for song, output in zip(songs, outputs):
-    _, file, difficulty, _, _, _, _ = song
+    _, file, difficulty, instrument, _, _, _, _ = song
     result = subprocess.run(
-        [program, "-f", f"{song_dir}/{file}", "-d", difficulty],
+        [program, "-f", f"{song_dir}/{file}",
+            "-d", difficulty, "-i", instrument],
         capture_output=True
     )
     result.check_returncode()
