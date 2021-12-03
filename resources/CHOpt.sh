98e6a51d25c02edd961d2ee2dc4821877489ddd3
Raymond Wright <rayw1710@gmail.com>
2021-12-03 15:47:52 +0000

    Add builds on tag to AppVeyor

---
+++ resources/CHOpt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This script is taken from https://doc.qt.io/qt-6/linux-deployment.html and its
+# purpose is to let CHOpt find the dynamic libraries included along with it.
+appname=`basename $0 | sed s,\.sh$,,`
+
+dirname=`dirname $0`
+tmp="${dirname#?}"
+
+if [ "${dirname%$tmp}" != "/" ]; then
+dirname=$PWD/$dirname
+fi
+LD_LIBRARY_PATH=$dirname/"libs"
+export LD_LIBRARY_PATH
+$dirname/$appname "$@"
