d9b081a7b6634166d86fb7c9818af3336fca3b29
Raymond Wright <rayw1710@gmail.com>
2023-03-12 23:10:04 +0000

    Address CHOpt.sh complaints

--- resources/CHOpt.sh
+++ resources/CHOpt.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This script is taken from https://doc.qt.io/qt-6/linux-deployment.html and its
 # purpose is to let CHOpt find the dynamic libraries included along with it.
-appname=`basename $0 | sed s,\.sh$,,`
+appname=$(basename "$0" | sed s,\.sh$,,)
 
-dirname=`dirname $0`
+dirname=$(dirname "$0")
 tmp="${dirname#?}"
 
 if [ "${dirname%$tmp}" != "/" ]; then
@@ -11,4 +11,4 @@ dirname=$PWD/$dirname
 fi
 LD_LIBRARY_PATH=$dirname/"libs"
 export LD_LIBRARY_PATH
-$dirname/$appname "$@"
+"$dirname/$appname" "$@"
