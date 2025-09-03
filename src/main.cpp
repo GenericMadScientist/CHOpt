a3b94c7124852bbbb9c016a36ed1ac6eb7cd1e5f
Raymond Wright <gms@gmscorner.dev>
2025-09-03 22:51:00 +0100

    Increase version number to 1.10.0

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
     try {
         QCoreApplication app {argc, argv};
         QCoreApplication::setApplicationName("CHOpt");
-        QCoreApplication::setApplicationVersion("1.9.5");
+        QCoreApplication::setApplicationVersion("1.10.0");
 
         const auto settings = from_args(QCoreApplication::arguments());
         const SongFile song_file {settings.filename};
